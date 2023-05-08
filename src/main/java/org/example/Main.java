db38dfb4dd3a7300100a3b99325753d59a6d3eb6
eldar <eldar.zaripoff@gmail.com>
2023-05-08 15:54:19 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server started");
+        int port = 6320;
+
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+                {
+                    System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+
+                    out.println("What's your name?");
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d. How do you feel?", name, clientSocket.getPort()));
+                    final String condition = in.readLine();
+                    out.println(String.format("You replied :%s. But if you need help, call to our hotline number 922", condition));
+                }
+
+            }
+
+        }
+    }
+}
